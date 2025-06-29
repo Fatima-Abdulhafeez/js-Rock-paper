7a0cdf9bb4c9d5fc8e932d7ee32a3d85f666b531
fatimahz-ga <fatimaabdullhafeez@gmail.com>
2025-06-29 10:44:53 +0300

    updates

---
+++ js/app.js
@@ -0,0 +1,24 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const choices = ['rock', 'paper', 'scissors'];
+
+/*-------------------------------- Variables --------------------------------*/
+
+let playerChoice;
+let computerChoice; 
+let msg;
+
+/*------------------------ Cached Element References ------------------------*/
+
+const rockbtn = document.querySelector("#rock");
+console.log(rockbtn);
+const paperbtn = document.querySelector("#paper");
+console.log(paperbtn);
+const secisorbtn = document.querySelector("#scissors");
+console.log(secisorbtn);
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
