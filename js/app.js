15370e57f04808e2513e8f9d3026e42c43be9394
fatimahz-ga <fatimaabdullhafeez@gmail.com>
2025-06-29 11:09:33 +0300

    added functions

--- js/app.js
+++ js/app.js
@@ -18,7 +18,31 @@ const secisorbtn = document.querySelector("#scissors");
 console.log(secisorbtn);
 
 /*-------------------------------- Functions --------------------------------*/
+function getComputerChoice(){
+    //genarate random nuber 0-2 for array
+    const ransomIndex = Math.floor(Math.random()*choices.length);
+    
+    console.log(ransomIndex)
+    // selects the item from the array
 
+}
 
+getComputerChoice();
+
+function play(){
+    
+    computerChoice = getComputerChoice();
+    console.log(computerChoice);
+    getComputerChoice();
+    console.log("co",computerChoice);
+    //after updating state, render to html
+    render()
+}
+play();
+
+//updates our ui 
+function render(){
+
+}
 
 /*----------------------------- Event Listeners -----------------------------*/
